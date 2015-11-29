a98a21d5ce6827ce46963173e613e81988be930e
Silvia Trevellin <silvia.trevellin@gmail.com>
2015-11-29 19:12:01 +0100

    Created basic level application
    You can add new articles (clothes) using postman.
    To modified an attribute you can use postman: GET the id  - POST  Row and Json and modified the attribute.
    To delete: GET the id  - DEELTE

---
+++ api/controllers/DressController.js
@@ -0,0 +1,24 @@
+/**
+ * DressController
+ *
+ * @description :: Server-side logic for managing Dresses
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	today: function(req, res) {
+
+    Dress.find().then(function(listOfDresses) {
+
+      console.log("=======", listOfDresses.length, listOfDresses);
+
+      res.view('homepage', {
+        numOfDresses: listOfDresses.length,
+        listOfDresses: listOfDresses
+      });
+
+    });
+
+  }
+};
+
